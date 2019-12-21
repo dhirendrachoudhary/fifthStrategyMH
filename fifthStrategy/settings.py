4cb3ee6fce7175a4c96afbd5b0ecd0b3bfee6edf
Dhirendra Kumar Choudhary <dhirendrachoudhary@yahoo.com>
2019-12-22 05:27:21 +0530

    Update settings.py

--- fifthStrategy/settings.py
+++ fifthStrategy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l^!ioga!_rii51k#c2ltchm1at$4!uq7^a%4vv_pfqir(+xcc#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
