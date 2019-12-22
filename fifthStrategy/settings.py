0a61fba659ae1a538bb718941b3d88f61117fb4c
Dhirendra Kumar Choudhary <dhirendrachoudhary@yahoo.com>
2019-12-22 06:35:56 +0530

    Update settings.py

--- fifthStrategy/settings.py
+++ fifthStrategy/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
-STATIC_ROOT = os.path.join(PROJECT_DIR, STATIC_URL)
+STATIC_ROOT = os.path.join(PROJECT_DIR, '/static/')
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,4 +122,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
